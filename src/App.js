dc4aca297ba532ae01bfa6020a78f18e4fa811de
Thiện Lâm <lamminhthien02012000@gmail.com>
2022-12-14 10:52:09 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+import { MultipleContainers } from "./MultipleContainers";
+
+export default function App() {
+  return (
+    <MultipleContainers
+      containerStyle={{
+        maxHeight: "80vh"
+      }}
+      itemCount={15}
+      scrollable
+    />
+  );
+}
